591f09920ea680243e5dd460bd63826374827228
hchengsz <56488002+hchengsz@users.noreply.github.com>
2021-05-23 18:27:14 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addMovieAction,
+  loadMovieDataAction,
+  removeMovieAction,
+} from "./actions";
+import List from "./components/List";
+import "./index.css";
+
+function App() {
+  const dispatch = useDispatch();
+  const { myList, recommendations, loading } = useSelector((state) => {
+    return state.list;
+  });
+  useEffect(() => {
+    dispatch(loadMovieDataAction());
+  }, [dispatch]);
+
+  const handleAdd = (movie) => {
+    dispatch(addMovieAction(movie));
+  };
+
+  const handleRemove = (movie) => {
+    dispatch(removeMovieAction(movie.id));
+  };
+  return (
+    <div>
+      <img
+        className="logo"
+        src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo-700x394.png"
+      />
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <Fragment>
+          <List
+            title="Recommendations"
+            data={recommendations}
+            buttonText="Add"
+            onButtonClick={handleAdd}
+          />
+          <hr />
+          <List
+            title="My List"
+            data={myList}
+            buttonText="Remove"
+            onButtonClick={handleRemove}
+          />
+        </Fragment>
+      )}
+    </div>
+  );
+}
+
+export default App;
